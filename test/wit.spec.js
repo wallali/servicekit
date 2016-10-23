9a342954fbb2e68e2d2606ceea4b88f06b85b9cd
Ali Lokhandwala <ali@huestones.co.uk>
2016-10-24 00:22:25 +0100

    Added wit service.

---
+++ test/wit.spec.js
@@ -0,0 +1,245 @@
+/**
+ * Copyright (c) 2016 Ali Lokhandwala <ali@huestones.co.uk>. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const assert = require('assert');
+const nock = require('nock');
+var factory = require('../wit');
+
+describe('wit', function () {
+
+  before(function () {
+    nock.disableNetConnect();
+  });
+
+  after(function () {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  var config = {
+    accessToken: 'FBACCESSTOKEN',
+    userTimezone: 'Europe/London',
+    apiVersion: '20160516'
+  };
+
+  var context = {};
+
+  const witUrl = 'https://api.wit.ai';
+
+  beforeEach(function () {
+    config = {
+      accessToken: 'FBACCESSTOKEN',
+      userTimezone: 'Europe/London',
+      apiVersion: '20160516'
+    };
+
+    context = {};
+  });
+
+  it('calls /message', function (done) {
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(function (q) {
+        return q.q === 'sample message';
+      })
+      .reply(200, {});
+
+    let wit = factory(config);
+
+    wit.message('sample message', context, function (e, r) {
+      assert(r);
+      assert(!e);
+
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('calls /message and callbacks when no context supplied', function (done) {
+    config.userTimezone = null;
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(function (q) {
+        return q.context === '{}';
+      })
+      .reply(200, {});
+
+    let wit = factory(config);
+
+    wit.message('sample message', function (e, r) {
+      assert(r);
+      assert(!e);
+
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('uses correct headers', function (done) {
+
+    let scope = nock(witUrl)
+      .matchHeader('accept', 'application/vnd.wit.20160516+json')
+      .matchHeader('authorization', 'Bearer FBACCESSTOKEN')
+      .get('/message')
+      .query(true)
+      .reply(200, {});
+
+    let wit = factory(config);
+
+    wit.message('sample message', context, function (e, r) {
+      assert(r);
+      assert(!e);
+
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('uses apiVersion from config', function (done) {
+
+    config.apiVersion = '010101';
+    config.userTimezone = null;
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(function (q) {
+        return q.v === config.apiVersion;
+      })
+      .reply(200, {});
+
+    let wit = factory(config);
+
+    wit.message('message', context, function (e, r) {
+      assert(r);
+      assert(!e);
+
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('uses userTimezone from config', function (done) {
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(function (q) {
+        return q.context === '{"timezone":"Europe/London"}';
+      })
+      .reply(200, {});
+
+    let wit = factory(config);
+
+    wit.message('message', context, function (e, r) {
+      assert(r);
+      assert(!e);
+
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('uses userTimezone from context', function (done) {
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(function (q) {
+        return q.context === '{"timezone":"newtime"}';
+      })
+      .reply(200, {});
+
+    let wit = factory(config);
+
+    context.timezone = 'newtime';
+    wit.message('message', context, function (e, r) {
+      assert(r);
+      assert(!e);
+
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('on error returns error', function (done) {
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(true)
+      .replyWithError('not found');
+
+    let wit = factory(config);
+
+    wit.message('sample message', context, function (e, r) {
+      assert(!r);
+      assert(e);
+
+      assert.strictEqual(e.message, 'not found');
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('on error returns error detail', function (done) {
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(true)
+      .reply(400, {
+        error: 'error detail'
+      });
+
+    let wit = factory(config);
+
+    wit.message('sample message', context, function (e, r) {
+      assert(!r);
+      assert(e);
+
+      assert.strictEqual(e.message, 'error detail');
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+
+  it('treats non 200 response as error', function (done) {
+
+    let scope = nock(witUrl)
+      .get('/message')
+      .query(true)
+      .reply(400, {});
+
+    let wit = factory(config);
+
+    wit.message('sample message', context, function (e, r) {
+      assert(!r);
+      assert(e);
+
+      assert.strictEqual(e.message, 'Unexpected response status 400');
+      assert(scope.isDone());
+
+      done();
+    });
+  });
+});
---
+++ wit.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2016 Ali Lokhandwala <ali@huestones.co.uk>. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const request = require('request');
+const noop = function () {};
+var debug = require('debug')('servicekit:wit');
+
+/** 
+ * The wit service wrapper factory.
+ * @param {Object} config Configuration for the wit service.
+ * @param {string} config.accessToken Wit access token for your app.
+ * @param {string} config.apiVersion, optional
+ * @param {string} config.userTimezone, optional. Canonical timezone (http://joda-time.sourceforge.net/timezones.html) 
+ * @return {Function}
+ */
+module.exports = function create(config) {
+
+  var apiVersion = config.apiVersion || '20160516';
+
+  var message = function (text, context, cb) {
+    if (typeof (context) === 'function') {
+      cb = context;
+      context = {};
+    }
+
+    if (!cb) cb = noop;
+
+    var ctx = context || {};
+
+    if (config.userTimezone && !ctx.timezone && !ctx.reference_time) {
+      ctx.timezone = config.userTimezone;
+      debug('Setting timezone in context', config.userTimezone);
+    }
+
+    var handleResponse = function (error, response, body) {
+      if (error) return cb(error);
+
+      if (response.statusCode !== 200) {
+        let err = new Error('Unexpected response status ' + response.statusCode);
+        if (body && body.error) {
+          err.message = body.error;
+        }
+        return cb(err);
+      }
+
+      cb(null, body);
+    };
+
+    let options = {
+      url: 'https://api.wit.ai/message',
+      method: 'GET',
+      json: true,
+      qs: {
+        v: apiVersion,
+        q: text,
+        context: JSON.stringify(ctx)
+      },
+      headers: {
+        'Accept': 'application/vnd.wit.' + apiVersion + '+json'
+      }
+    };
+
+    request.get(options, handleResponse).auth(null, null, true, config.accessToken);
+  };
+
+  return {
+    message: message
+  };
+};
